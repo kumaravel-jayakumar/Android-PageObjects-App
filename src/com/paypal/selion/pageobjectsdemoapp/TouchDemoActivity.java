39d19a53e4d24487c84f8b4b783df5ec596518c9
Kumaravel Jayakumar <kjayakumar@paypal.com>
2015-04-23 19:54:01 +0530

    Added scroll page for demo app

--- src/com/paypal/selion/pageobjectsdemoapp/TouchDemoActivity.java
+++ src/com/paypal/selion/pageobjectsdemoapp/TouchDemoActivity.java
@@ -1,45 +1,92 @@
 package com.paypal.selion.pageobjectsdemoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 public class TouchDemoActivity extends ActionBarActivity {
-
-	private GestureDetector gestureDetector;
-
-	private Button doubleTouchButton;
-
-	private TextView doubleTouchTextView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_touch_demo);
-		gestureDetector = new GestureDetector(this, new GestureListener());
-		doubleTouchButton = (Button) findViewById(R.id.double_touch_button);
-		doubleTouchTextView = (TextView) findViewById(R.id.double_touch_button_output);
-		setupDoubleTouchButton();
+		setUpAndroidCheckBox();
+		setUpIosCheckBox();
+		setUpSeekBar();
+	}
+
+	private void setUpSeekBar() {
+		
+		SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+		seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				TextView seekBar_textview = (TextView) findViewById(R.id.seekBar_textview);
+				seekBar_textview.setText("Value: " + progress);
+			}
+		});
+		
+	}
+
+	private void setUpAndroidCheckBox() {
+		final CheckBox androidCheckBox = (CheckBox) findViewById(R.id.android_checkbox);
+		androidCheckBox.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (androidCheckBox.isChecked()) {
+					TextView checkBox_textview = (TextView) findViewById(R.id.checkbox_textview);
+					checkBox_textview.setText("Android");
+				} else {
+					TextView checkBox_textview = (TextView) findViewById(R.id.checkbox_textview);
+					checkBox_textview.setText("");
+				}
+			}
+		});
+	}
+
+	private void setUpIosCheckBox() {
+		final CheckBox iOSCheckBox = (CheckBox) findViewById(R.id.ios_checkbox);
+		iOSCheckBox.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (iOSCheckBox.isChecked()) {
+					TextView checkBox_textview = (TextView) findViewById(R.id.checkbox_textview);
+					checkBox_textview.setText("iOS");
+				} else {
+					TextView checkBox_textview = (TextView) findViewById(R.id.checkbox_textview);
+					checkBox_textview.setText("");
+				}
+			}
+		});
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		doubleTouchTextView.setText("");
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.page_objects_home, menu);
 		invalidateOptionsMenu();
-		menu.findItem(R.id.action_button).setTitle("Slider");
+		menu.findItem(R.id.action_button).setTitle("Scroll");
 		return super.onCreateOptionsMenu(menu);
 	}
 
@@ -47,40 +94,11 @@ public class TouchDemoActivity extends ActionBarActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		int id = item.getItemId();
 		if (id == R.id.action_button) {
+			Intent intent = new Intent(this, ScrollActivity.class);
+			startActivity(intent);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-	private void setupDoubleTouchButton() {
-		doubleTouchButton.setOnTouchListener(new View.OnTouchListener() {
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				Log.i("TouchDemoActivity", "on touch called");
-				boolean doubleTapped = gestureDetector.onTouchEvent(event);
-				if (doubleTapped) {
-					handleDoubleTouch(v);
-				}
-				return doubleTapped;
-			}
-		});
-	}
 
-	private void handleDoubleTouch(View v) {
-		doubleTouchTextView.setText("Touch Count: 2");
-	}
-	
-	public static class GestureListener extends
-			GestureDetector.SimpleOnGestureListener {
-
-		@Override
-		public boolean onDown(MotionEvent e) {
-			if (e.getPointerCount() == 2) {
-				Log.i("Touch Demo Activity", "Double touch press");
-				return true;
-			}
-			return false;
-		}
-
-	}
 }
